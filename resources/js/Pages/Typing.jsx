08ea23403bb35d558a062109eb2cac495736e41f
p-obrthr <pascal.oberthuer@web.de>
2023-11-26 16:49:09 +0100

    add new functionality

--- resources/js/Pages/Typing.jsx
+++ resources/js/Pages/Typing.jsx
@@ -26,7 +26,7 @@ function Word(props) {
 Word = React.memo(Word);
 
 function Timer(props) {
-    const { correctWords, startCounting, timeElapsed, setTimeElapsed } = props;
+    const { correctWords, startCounting, timeElapsed, setTimeElapsed, totalWords} = props;
 
     useEffect(() => {
         if (startCounting) {
@@ -41,22 +41,36 @@ function Timer(props) {
     }, [startCounting, setTimeElapsed]);
 
     const minutes = timeElapsed / 60;
+    const accuracyRate = totalWords > 0 ? ((correctWords / totalWords) * 100).toFixed(1) : 0;
 
     return (
-        <div className="row">
-            <div className="col-md-6">
+        <div className="row d-flex align-items-center justify-content-center">
+            <div className="col-md-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 16">
                     <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z" />
                     <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3" />
                 </svg>
-                <p style={{ display: "inline-block", marginLeft: "5px" }}>Time: {timeElapsed}s</p>
+                <p style={{ display: "inline-block", marginLeft: "5px", color: timeElapsed > 0 ? "var(--blue_one)" : "aliceblue"}}>
+                     Time: {timeElapsed} s
+                </p>
             </div>
-            <div className="col-md-6 text-left">
+            <div className="col-md-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-speedometer2" viewBox="0 0 16 16">
                     <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4M3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z" />
                     <path fillRule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10m8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3" />
                 </svg>
-                <p style={{ display: "inline-block", marginLeft: "5px" }}>Speed: {((correctWords / minutes) || 0).toFixed(0)} WPM</p>
+                <p style={{ display: "inline-block", marginLeft: "5px", color: timeElapsed > 0 ? "var(--blue_one)" : "aliceblue" }}>
+                    Speed: {((correctWords / minutes) || 0).toFixed(0)} WPM
+                </p>
+            </div>
+            <div className="col-md-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-data-fill" viewBox="0 0 16 16">
+                <path d="M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"/>
+                 <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5zM10 8a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0zm-6 4a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0zm4-3a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1"/>
+            </svg>
+                <p style={{ display: "inline-block", marginLeft: "5px", color: timeElapsed > 0 ? "var(--blue_one)" : "aliceblue" }}>
+                    Accuracy: {((accuracyRate))} %
+                </p>
             </div>
         </div>
     );
@@ -70,6 +84,8 @@ function App() {
     const [ipsum, setIpsum] = useState("baconipsum");
     const [startCounting, setStartCounting] = useState(false);
     const [timeElapsed, setTimeElapsed] = useState(0);
+    const [totalWords, setTotalWords] = useState(0);
+    const [showAlert, setShowAlert] = useState(false);
 
     function getText() {
         reset();
@@ -81,6 +97,9 @@ function App() {
         setCorrectWordArray([]);
         setStartCounting(false);
         setTimeElapsed(0);
+        setUserInput('');
+        setTotalWords(0);
+        setShowAlert(false);
     }
 
     function processInput(value) {
@@ -89,20 +108,22 @@ function App() {
         }
 
         if (value.endsWith(' ')) {
-            if (activeWordIndex === generatedText.length - 1) {
-                setStartCounting(false);
-                return;
-            }
-
-            setActiveWordIndex(index => index + 1);
-            setUserInput('');
-
             setCorrectWordArray(data => {
                 const word = value.trim();
                 const newResult = [...data];
                 newResult[activeWordIndex] = word === generatedText[activeWordIndex];
                 return newResult;
             });
+            if (activeWordIndex === generatedText.length - 1) {
+                setStartCounting(false);
+                setUserInput('');
+                setShowAlert(true);
+                return;
+            }
+            setTotalWords(count => count + 1);
+            setActiveWordIndex(index => index + 1);
+            setUserInput('');
+
         } else {
             setUserInput(value);
         }
@@ -209,6 +230,7 @@ function App() {
                         correctWords={correctWordArray.filter(Boolean).length}
                         timeElapsed={timeElapsed}
                         setTimeElapsed={setTimeElapsed}
+                        totalWords={totalWords}
                     />
                 </div>
 
@@ -225,6 +247,12 @@ function App() {
                     </p>
                 </div>
             </div>
+            {showAlert && (
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <strong>Holy guacamole!</strong> Wow, that was a good run.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>)
+            }
             <input
                 type="text"
                 className="form-control select-container custom-select my-1 mr-sm-2 userinput"
